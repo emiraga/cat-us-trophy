8b127ab5ee94a561137c34fcf5bc6ad1ff9fb238
Emir Habul <emiraga@gmail.com>
2010-12-07 11:31:00 +0800

    Working on geometry

--- notebook/src/geom/cpp-geom.cpp
+++ notebook/src/geom/cpp-geom.cpp
@@ -1,81 +1,150 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
+#include <cassert>
 using namespace std;
-
+double eps = 1e-8;
+struct point2d {
+    double x,y;
+	point2d operator-(point2d v) { return (point2d){x-v.x, y-v.y}; }
+	point2d operator+(point2d v) { return (point2d){x+v.x, y+v.y}; }
+};
 struct point3d {
     double x,y,z;
-    point operator-(point v) { return point(x-v.x, y-v.y, z-v.z); }
+    point3d operator-(point3d v) { return (point3d){x-v.x, y-v.y, z-v.z}; }
 };
-
-//include
-
 double trip(point3d a, point3d b, point3d c) {
     return a.x * ( b.y * c.z - b.z * c.y )
         - a.y *  ( b.x * c.z - b.z * c.x )
         + a.z *  ( b.x * c.y - b.y * c.x );
 }
-
+double len(point2d a) { return sqrt(a.x*a.x+a.y*a.y); }
 double len(point3d a) { return sqrt(a.x*a.x+a.y*a.y+a.z*a.z); }
-
 point3d cross(point3d a, point3d b) {
-	return point(
+	return (point3d){
             a.y*b.z - a.z*b.y,
             a.z*b.x - a.x*b.z,
-            a.x*b.y - a.y*b.x);
+            a.x*b.y - a.y*b.x};
 }
-
 double dot(point3d a, point3d b) { return a.x*b.x + a.y*b.y + a.z*b.z; }
-
-    T operator%(const Vec<2> &a)const{assert(d==2);return c[0]*a[1]-c[1]*a[0]; }//2d cross product
-    Vec operator%(const Vec<3> &a)const{assert(d==3);return Vec(c[1]*a[2]-c[2]*a[1],c[2]*a[0]-c[0]*a[2],c[0]*a[1]-c[1]*a[0]);}//3d cross
-    Vec rotate(LD angle) {assert(d==2);return Vec(c[0]*cos(angle)-c[1]*sin(angle),c[0]*sin(angle)+c[1]*cos(angle));}
-    bool operator<(const Vec &v)const{ Rep(i,d)if(c[i]!=v[i])return c[i]<v[i];return false; }
-
-LD linePointDist(Vec<> A, Vec<> B, Vec<> C, bool isSegment){
-    LD dist = ((B-A)%(C-A)) / sqrt((B-A)*(B-A));
+double dot(point2d a, point2d b) { return a.x*b.x + a.y*b.y; }
+double cross(point2d a, point2d b) {
+	return a.x*b.y - a.y*b.x;
+}
+point2d rotate(point2d c, double angle) {
+	return (point2d) {
+	    c.x*cos(angle)-c.y*sin(angle),
+		c.x*sin(angle)+c.y*cos(angle) };
+}
+//distance AB to C
+double linePointDist(point2d A, point2d B, point2d C, bool isSegment){
+    double dist = cross(B-A,C-A) / len(B-A);
     if(isSegment){
-        if( (C-B)*(B-A) > eps) return (B-C).len();
-        if( (C-A)*(A-B) > eps) return (A-C).len();
+        if( dot(C-B,B-A) > eps) return len(B-C);
+        if( dot(C-A,A-B) > eps) return len(A-C);
     }
-    return iabs(dist);
+    return fabs(dist);
 }
-
-bool segmentsIntersect( Vec<> A, Vec<> B, Vec<> C, Vec<> E )
+struct Line { double A,B,C; };//Ax + By = C
+Line makeline(point2d a1, point2d a2)
 {
-    Vec<> in = Line<>(A,B).intersect(Line<>(C,E));
-        return linePointDist(A,B,in,true) < eps && linePointDist(C,E,in,true) < eps;
+	Line ret = (Line){ a2.y-a1.y, a1.x-a2.x };
+	ret.C = ret.A * a1.x + ret.B * a1.y;
+	return ret;
 }
-
-template<class T=LD> struct Line
+double dist(Line l, point2d p)
 {
-    T A,B,C;
-    Line(){A=B=C=0;}
-    Line(T x1,T y1,T x2,T y2) { A=y2-y1; B=x1-x2; C=A*x1+B*y1; }//construct from 4 values
-    T dist(T a0, T a1) const { return iabs( a0*A + a1*B - C ) / sqrt(A*A+B*B); }
-    Line rot90(T x,T y)const{Line ret;ret.A=-B;ret.B=A;ret.C=ret.A*x+ret.B*y;return ret;}//rot line
-    friend ostream& operator<<(ostream& os,const Line &a){return os<<a.A<<"*x + "<<a.B<<"*y = "<<a.C; }
-    //requires Vec
-    Line(Vec<2,T>a,Vec<2,T>b){A=b[1]-a[1];B=a[0]-b[0];C=A*a[0]+B*a[1];} //construct from two points
-    T dist(Vec<2,T> a) const { return ( a[0]*A + a[1]*B - C ) / sqrt(A*A+B*B); }
-    Vec<2,T> intersect(const Line &l) const {T det = A*l.B - l.A*B; if(iabs(det) < eps)det=0; return Vec<2,T>((l.B*C - B*l.C)/det, (A*l.C - l.A*C)/det); }
-};
+	return fabs(p.x * l.A + p.y * l.B - l.C)/sqrt(l.A*l.A+l.B*l.B);;
+}
+Line rot90(Line l, point2d p)
+{
+	Line ret = (Line){ -l.B, l.A };
+	ret.C = ret.A * p.x + ret.B * p.y;
+	return ret;
+}
+point2d intersect(Line l1, Line l2)
+{
+	double det = l1.A*l2.B - l2.A*l1.B;
+	if(fabs(det) < eps) det=0;//zero means parallel
+	return (point2d) { (l2.B*l1.C - l1.B*l2.C)/det, (l1.A*l2.C - l2.A*l1.C)/det};
+}
 //for segment segment intersection, check additionally
 //min(x1,x2) <= x <= max(x1,x2)
-
-// get a line passing between two points
-template<class T>
-Line<T> getmidline(Vec<2,T> a, Vec<2,T> b)
+//min(y1,y2) <= x <= max(y1,y2)
+bool segmentsIntersect( point2d A, point2d B, point2d C, point2d E )
 {
-    Vec<2,T> mid(a+b);
-        return Line<T>(a,b).rot90(mid[0]/2,mid[1]/2);
+	point2d in = intersect( makeline(A,B), makeline(C,E) );
+    return linePointDist(A,B,in,true) < eps && linePointDist(C,E,in,true) < eps;
 }
 
-//reflect a point into it's "mirror" with repect to a line
-template<class T>
-Vec<2,T> reflectPoint(Vec<2,T> p, Line<T> l)
+// get a line passing between two points
+Line getmidline(point2d a, point2d b)
 {
-    Line<T> r = l.rot90(p[0],p[1]);
-        Vec<2,T> Y=l.intersect(r);
-        return Y - (p-Y);
+    point2d mid(a+b); mid.x/=2; mid.y/=2;
+	return rot90( makeline(a,b), mid );
+}
+//reflect a point into it's "mirror" with repect to a line
+point2d reflectPoint(Line l, point2d p)
+{
+    Line r = rot90(l, p);
+    point2d Y=intersect(l,r);
+    return Y-(p-Y);
+}
+void printline(Line l1)
+{
+	cout << l1.A << "x + " << l1.B << "y = " << l1.C << endl;
+}
+void printpoint(point2d p)
+{
+	cout << "x=" << p.x << " y=" << p.y << endl;
+}
+
+double getr()
+{
+	return -10000 + rand()%20000;
+}
+double eq(double a, double b)
+{
+	return fabs(a-b)<eps;
+}
+
+int main()
+{
+	point2d A={1,0}, B={-3,-4};
+	point2d C={-1,-1};
+	Line l1 = makeline(A,B);
+	assert(dist(l1, A) < eps && dist(l1,B) < eps);
+
+	point2d A1={0,5}, B1={-5,0};
+	Line l2 = getmidline(A1,B1);
+	
+	point2d D = intersect(l1,l2);
+
+	for(int i=0;i<20000;i++)
+	{
+		point2d A={getr(), getr()}, B={getr(),getr()};
+		point2d C={getr(),getr()};
+		Line AB = makeline(A,B);
+		assert(eq(dist(AB,C),  linePointDist(A,B,C,false)));
+
+		Line mid = getmidline(A,B);
+		assert(eq( dist(mid, A), dist(mid,B) ));
+
+		point2d M = intersect(AB,mid);
+		assert(eq(M.x*2 , A.x+B.x));
+		assert(eq(M.y*2 , A.y+B.y));
+
+		point2d A2 = reflectPoint(mid, B);
+		point2d B2 = reflectPoint(mid, A);
+		assert( eq(A.x, A2.x) && eq(A.y, A2.y) );
+		assert( eq(B.x, B2.x) && eq(B.y, B2.y) );
+
+		point2d B3 = reflectPoint(AB, B);
+		point2d A3 = reflectPoint(AB, A);
+		assert( eq(A.x, A2.x) && eq(A.y, A2.y) );
+		assert( eq(B.x, B2.x) && eq(B.y, B2.y) );
+	}
+
+	return 0;
 }
 
