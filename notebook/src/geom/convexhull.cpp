f60152fca341970b1593c538939cbfeb4152e1dc
Emir Habul <emiraga@gmail.com>
2011-01-13 03:24:19 +0800

    Stirling numbers and convex hull

--- notebook/src/geom/convexhull.cpp
+++ notebook/src/geom/convexhull.cpp
@@ -1,6 +1,10 @@
-LL cross(const Point &O, const Point &A, const Point &B) {
-return (A.x-O.x) * (B.y-O.y) - (A.y-O.y) * (B.x-O.x); }
-// convex hull in counter-clockwise order. the last point is the same as the first one.
+struct Point { LL x,y; bool operator<(const Point &p)const{
+	return(x==p.x) ? y<p.y : x<p.x; } };
+LL cross(const Point &O, const Point &A, const Point &B)
+{ return (A.x-O.x) * (B.y-O.y) - (A.y-O.y) * (B.x-O.x); }
+// Returns a list of convex hull in counter-clockwise order.
+// In case of collinear points, only end-points are taken (not middle).
+// The last point is the same as the first one.
 vector<Point> ConvexHull(vector<Point> P) {
     int n = P.size(), k = 0; vector<Point> H(2*n);
     sort(P.begin(), P.end());
@@ -8,4 +12,4 @@ vector<Point> ConvexHull(vector<Point> P) {
     { while (k >= 2 && cross(H[k-2], H[k-1], P[i]) <= 0) k--; H[k++] = P[i]; }
     for (int i = n-2, t = k+1; i >= 0; i--)
     { while (k >= t && cross(H[k-2], H[k-1], P[i]) <= 0) k--; H[k++] = P[i]; }
-    H.resize(k); return H;  }
+    H.resize(k); return H; }
